752c2abe530bc66e633516bfa1f8cd1937871d2d
André <andrebq@gmail.com>
2022-02-09 20:03:47 +0100

    chore: duplicate code because there is no generic

--- broker_test.go
+++ broker_test.go
@@ -20,7 +20,49 @@ func testIntPublish(t *testing.T, expectedValue int) {
 			t.Error("unable to receive data", err)
 			return
 		}
+		// Typecheck required because the underlying object is an interface
 		if actualValue, ok := d.(int); !ok {
+			t.Error("Invalid type")
+		} else if actualValue != expectedValue {
+			t.Errorf("Subscription %p got %v instead of %v", out, d, expectedValue)
+		}
+		out <- d
+	}
+
+	go waitForData(sub1)
+	go waitForData(sub2)
+
+	// wait for a few moments, otherwise one of the subscribers
+	// might subscribe after the message has been published
+	go func() {
+		// 1ms should be enough time for the previous 2 goroutines
+		// to subscribe to the broker
+		time.Sleep(time.Millisecond)
+		if err := broker.Publish(ctx, expectedValue); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	<-sub1
+	<-sub2
+}
+
+func testFloat64Publish(t *testing.T, expectedValue float64) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	broker := NewRunningBroker(ctx, 1)
+	sub1, sub2 := make(chan D), make(chan D)
+
+	waitForData := func(out chan D) {
+		defer close(out)
+		d, err := broker.WaitForData(ctx)
+		if err != nil {
+			t.Error("unable to receive data", err)
+			return
+		}
+		// Typecheck required because the underlying object is an interface
+		if actualValue, ok := d.(float64); !ok {
 			t.Error("Invalid type")
 		} else if actualValue != expectedValue {
 			t.Errorf("Subscription %p got %v instead of %v", out, d, expectedValue)
@@ -48,4 +90,5 @@ func testIntPublish(t *testing.T, expectedValue int) {
 
 func TestBroker(t *testing.T) {
 	testIntPublish(t, 1)
+	testFloat64Publish(t, 1.0)
 }
