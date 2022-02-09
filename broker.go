0f64f3d9e367ab1fba57a00834fd378c5b5b0247
André <andrebq@gmail.com>
2022-02-09 20:18:10 +0100

    feat: make lib code generic

--- broker.go
+++ broker.go
@@ -8,23 +8,25 @@ import (
 
 type (
 	// D represents the 'D'ata that can be sent over a PubSub channel.
-	D interface{}
+	D interface {
+		int | float64
+	}
 
 	// Broker implements the main pubsub logic
-	Broker struct {
-		incomingData chan D
+	Broker[Datatype D] struct {
+		incomingData chan Datatype
 		done         chan empty
 		stop         chan empty
 
 		closeErr error
 
-		subscribe   chan subscription
-		unsubscribe chan subscription
+		subscribe   chan subscription[Datatype]
+		unsubscribe chan subscription[Datatype]
 	}
 
 	// helper types to make the code a bit more readable
-	subscription chan D
-	subscribers  map[subscription]empty
+	subscription[Datatype D] chan Datatype
+	subscribers[Datatype D]  map[subscription[Datatype]]empty
 	empty        struct{}
 )
 
@@ -40,30 +42,30 @@ var (
 // of all subscriptions.
 //
 // If buffer < 0, buffer = runtime.NumCPU()
-func NewBroker(buffer int) *Broker {
+func NewBroker[Datatype D](buffer int) *Broker[Datatype] {
 	if buffer < 0 {
 		buffer = runtime.NumCPU()
 	}
-	return &Broker{
-		incomingData: make(chan D, buffer),
+	return &Broker[Datatype]{
+		incomingData: make(chan Datatype, buffer),
 		done:         make(chan empty),
 		stop:         make(chan empty),
-		subscribe:    make(chan subscription, runtime.NumCPU()),
-		unsubscribe:  make(chan subscription, runtime.NumCPU()),
+		subscribe:    make(chan subscription[Datatype], runtime.NumCPU()),
+		unsubscribe:  make(chan subscription[Datatype], runtime.NumCPU()),
 	}
 }
 
 // NewRunningBroker returns a broker which is running on a background goroutine,
 // it is equivalent to `go NewBroker(buffer).Run(ctx)`
-func NewRunningBroker(ctx context.Context, buffer int) *Broker {
-	b := NewBroker(buffer)
+func NewRunningBroker[Datatype D](ctx context.Context, buffer int) *Broker[Datatype] {
+	b := NewBroker[Datatype](buffer)
 	go b.Run(ctx)
 	return b
 }
 
 // Run the broker until ctx is closed
-func (b *Broker) Run(ctx context.Context) error {
-	subs := make(subscribers)
+func (b *Broker[Datatype]) Run(ctx context.Context) error {
+	subs := make(subscribers[Datatype])
 	b.closeErr = errBrokerIsClosed
 	defer func() {
 		close(b.done)
@@ -85,22 +87,23 @@ func (b *Broker) Run(ctx context.Context) error {
 
 // WaitForData from one of the producers or until the context is done
 // or Close() error ins called in the Broker
-func (b *Broker) WaitForData(ctx context.Context) (D, error) {
+func (b *Broker[Datatype]) WaitForData(ctx context.Context, out *Datatype) error {
 	sub, err := b.doSubscribe(ctx)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer b.doUnsubscribe(sub)
 	select {
 	case d, subscribed := <-sub:
 		if !subscribed {
-			return nil, errSubscriptionCancelled
+			return errSubscriptionCancelled
 		}
-		return d, nil
+		*out = d
+		return nil
 	case <-b.done:
-		return nil, b.closeErr
+		return b.closeErr
 	case <-ctx.Done():
-		return nil, ctx.Err()
+		return ctx.Err()
 	}
 }
 
@@ -109,7 +112,7 @@ func (b *Broker) WaitForData(ctx context.Context) (D, error) {
 //
 // Publish returns as soon as the broker receives the data but before the data is delivered
 // to all consumers. There is no guarantee that data will eventually reach consumers.
-func (b *Broker) Publish(ctx context.Context, data D) error {
+func (b *Broker[Datatype]) Publish(ctx context.Context, data Datatype) error {
 	select {
 	case <-b.done:
 		return b.closeErr
@@ -121,7 +124,7 @@ func (b *Broker) Publish(ctx context.Context, data D) error {
 }
 
 // Close the broker and waits until the broker has completed its shutdown process
-func (b *Broker) Close() error {
+func (b *Broker[Datatype]) Close() error {
 	select {
 	case <-b.done:
 		return b.closeErr
@@ -131,15 +134,15 @@ func (b *Broker) Close() error {
 	return nil
 }
 
-func (b *Broker) doUnsubscribe(sub subscription) {
+func (b *Broker[Datatype]) doUnsubscribe(sub subscription[Datatype]) {
 	select {
 	case <-b.done:
 	case b.unsubscribe <- sub:
 	}
 }
 
-func (b *Broker) doSubscribe(ctx context.Context) (subscription, error) {
-	sub := make(subscription, 1)
+func (b *Broker[Datatype]) doSubscribe(ctx context.Context) (subscription[Datatype], error) {
+	sub := make(subscription[Datatype], 1)
 	select {
 	case <-b.done:
 		return nil, b.closeErr
@@ -148,14 +151,14 @@ func (b *Broker) doSubscribe(ctx context.Context) (subscription, error) {
 	return sub, nil
 }
 
-func (s subscribers) put(sub subscription) {
+func (s subscribers[Datatype]) put(sub subscription[Datatype]) {
 	_, has := s[sub]
 	if !has {
 		s[sub] = empty{}
 	}
 }
 
-func (s subscribers) del(sub subscription) {
+func (s subscribers[Datatype]) del(sub subscription[Datatype]) {
 	_, has := s[sub]
 	if has {
 		delete(s, sub)
@@ -163,13 +166,13 @@ func (s subscribers) del(sub subscription) {
 	}
 }
 
-func (s subscribers) clear() {
+func (s subscribers[Datatype]) clear() {
 	for sub := range s {
 		close(sub)
 	}
 }
 
-func (s subscribers) publish(ctx context.Context, value D) {
+func (s subscribers[Datatype]) publish(ctx context.Context, value Datatype) {
 	// TODO: think of better implementations, but for a proof-of-concept this is good enough
 	//
 	// Here, any subscriber that does not have capacity to receive the message will miss the
