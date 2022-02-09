d6b3f4bb121f7e9e9e8fbd1582d99056fd4bfd47
André <andrebq@gmail.com>
2022-02-09 20:18:22 +0100

    feat: make client code generic

--- broker_test.go
+++ broker_test.go
@@ -6,65 +6,22 @@ import (
 	"time"
 )
 
-func testIntPublish(t *testing.T, expectedValue int) {
+func testGenericPublish[Datatype D](t *testing.T, expectedValue Datatype) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	broker := NewRunningBroker(ctx, 1)
-	sub1, sub2 := make(chan D), make(chan D)
+	broker := NewRunningBroker[Datatype](ctx, 1)
+	sub1, sub2 := make(chan Datatype), make(chan Datatype)
 
-	waitForData := func(out chan D) {
+	waitForData := func(out chan Datatype) {
 		defer close(out)
-		d, err := broker.WaitForData(ctx)
+		var d Datatype
+		err := broker.WaitForData(ctx, &d)
 		if err != nil {
 			t.Error("unable to receive data", err)
 			return
 		}
-		// Typecheck required because the underlying object is an interface
-		if actualValue, ok := d.(int); !ok {
-			t.Error("Invalid type")
-		} else if actualValue != expectedValue {
-			t.Errorf("Subscription %p got %v instead of %v", out, d, expectedValue)
-		}
-		out <- d
-	}
-
-	go waitForData(sub1)
-	go waitForData(sub2)
-
-	// wait for a few moments, otherwise one of the subscribers
-	// might subscribe after the message has been published
-	go func() {
-		// 1ms should be enough time for the previous 2 goroutines
-		// to subscribe to the broker
-		time.Sleep(time.Millisecond)
-		if err := broker.Publish(ctx, expectedValue); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	<-sub1
-	<-sub2
-}
-
-func testFloat64Publish(t *testing.T, expectedValue float64) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	broker := NewRunningBroker(ctx, 1)
-	sub1, sub2 := make(chan D), make(chan D)
-
-	waitForData := func(out chan D) {
-		defer close(out)
-		d, err := broker.WaitForData(ctx)
-		if err != nil {
-			t.Error("unable to receive data", err)
-			return
-		}
-		// Typecheck required because the underlying object is an interface
-		if actualValue, ok := d.(float64); !ok {
-			t.Error("Invalid type")
-		} else if actualValue != expectedValue {
+		if d != expectedValue {
 			t.Errorf("Subscription %p got %v instead of %v", out, d, expectedValue)
 		}
 		out <- d
@@ -89,6 +46,6 @@ func testFloat64Publish(t *testing.T, expectedValue float64) {
 }
 
 func TestBroker(t *testing.T) {
-	testIntPublish(t, 1)
-	testFloat64Publish(t, 1.0)
+	testGenericPublish(t, int(1))
+	testGenericPublish(t, float64(1.0))
 }
